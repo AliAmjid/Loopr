817b51f19c79a02cdda66a29606c58aee5662c8f
Kryštof Řezáč <krystofrezac@gmail.com>
2020-08-03 22:40:54 +0200

    Add custom theme to storybook

--- app/src/lib/storybook/withDecorators.ts
+++ app/src/lib/storybook/withDecorators.ts
@@ -2,8 +2,10 @@ import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
 import { muiTheme } from 'storybook-addon-material-ui';
 
+import theme from 'lib/material-ui/theme';
+
 const withDecorators = (config: Record<string, any>): Record<string, any> => {
-  config.decorators = [muiTheme(), withKnobs, withA11y];
+  config.decorators = [muiTheme([theme]), withKnobs, withA11y];
 
   return config;
 };
