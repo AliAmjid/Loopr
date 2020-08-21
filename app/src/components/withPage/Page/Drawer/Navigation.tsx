75c904110b20cfb1d7fdd638d27776dc234778de
Kryštof Řezáč <krystofrezac@gmail.com>
2020-08-21 20:17:15 +0200

    Add selected listItem

--- app/src/components/withPage/Page/Drawer/Navigation.tsx
+++ app/src/components/withPage/Page/Drawer/Navigation.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { useTranslation } from 'lib/i18n';
 import namespaces from 'lib/i18n/namespaces';
@@ -10,9 +11,11 @@ import navigationList from 'components/withPage/Page/Drawer/navigationList';
 
 const Navigation = (): JSX.Element => {
   const { t } = useTranslation(namespaces.other.pages);
+  const router = useRouter();
+
   const mappedNavigation = navigationList.map(item => (
     <Link key={item.label + item.href} href={item.href} passHref>
-      <ListItem button>
+      <ListItem button selected={router.pathname === item.href}>
         <ListItemIcon>{item.icon}</ListItemIcon>
         <ListItemText>{t(item.label)}</ListItemText>
       </ListItem>
