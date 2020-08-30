d727475173fb961f853ce0041c8fb12b594cf1c0
Kryštof Řezáč <krystofrezac@gmail.com>
2020-08-30 12:19:55 +0200

    Add dialog when logging out

--- app/src/components/withPage/Page/AppBar/LogOut/index.tsx
+++ app/src/components/withPage/Page/AppBar/LogOut/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { IconButton, Tooltip } from '@material-ui/core';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+  IconButton,
+  Tooltip,
+} from '@material-ui/core';
 import LogOutIcon from '@material-ui/icons/PowerSettingsNew';
 
 import { useTranslation } from 'lib/i18n';
@@ -10,13 +17,35 @@ import { LogOutProps } from './types';
 
 const LogOut: React.FC<LogOutProps> = props => {
   const { t } = useTranslation(namespaces.components.withPage);
+  const [dialogOpen, setDialogOpen] = useState(false);
 
   return (
-    <Tooltip title={t<string>('appBar.logOut')}>
-      <IconButton color="inherit" onClick={props.onLogOut}>
-        <LogOutIcon />
-      </IconButton>
-    </Tooltip>
+    <>
+      <Tooltip title={t<string>('appBar.logOut')}>
+        <IconButton color="inherit" onClick={() => setDialogOpen(true)}>
+          <LogOutIcon />
+        </IconButton>
+      </Tooltip>
+      <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+        <DialogTitle>Opravdu se chcete odhlásit?</DialogTitle>
+        <DialogActions>
+          <Button
+            color="secondary"
+            variant="contained"
+            onClick={props.onLogOut}
+          >
+            Odhlásit se
+          </Button>
+          <Button
+            color="primary"
+            variant="contained"
+            onClick={() => setDialogOpen(false)}
+          >
+            Zpět
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
