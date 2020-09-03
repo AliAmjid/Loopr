d08019654bc328661b2752c9f8f3349b14ecc294
Kryštof Řezáč <krystofrezac@gmail.com>
2020-09-03 18:49:53 +0200

    Add OverlayLoadingContainer

--- app/src/pages/acl/editRole/editRole.tsx
+++ app/src/pages/acl/editRole/editRole.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import {
   Box,
   Button,
-  CircularProgress,
   Grid,
   Paper,
   TextField,
@@ -14,33 +13,37 @@ import Link from 'next/link';
 import routes from 'config/routes';
 
 import OverlayLoading from 'components/OverlayLoading';
+import OverlayLoadingContainer from 'components/OverlayLoading/OverlayLoadingContainer';
 
 const EditRole: React.FC = () => {
   return (
-    <Paper style={{ position: 'relative' }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h5">Role</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField variant="outlined" label="name" />
-        </Grid>
-        <Grid item container justify="flex-end" xs={12}>
-          <Box mr={2}>
-            <Button color="primary" variant="contained">
-              Uložit
-            </Button>
-          </Box>
-          <Grid item>
-            <Link href={routes.acl.index}>
-              <Button color="secondary" variant="contained">
-                Zrušit
+    <Paper>
+      <OverlayLoadingContainer>
+        <OverlayLoading loading />
+
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h5">Role</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField variant="outlined" label="name" />
+          </Grid>
+          <Grid item container justify="flex-end" xs={12}>
+            <Box mr={2}>
+              <Button color="primary" variant="contained">
+                Uložit
               </Button>
-            </Link>
+            </Box>
+            <Grid item>
+              <Link href={routes.acl.index}>
+                <Button color="secondary" variant="contained">
+                  Zrušit
+                </Button>
+              </Link>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <OverlayLoading loading />
+      </OverlayLoadingContainer>
     </Paper>
   );
 };
