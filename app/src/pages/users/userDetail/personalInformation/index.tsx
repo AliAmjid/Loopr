7451899cff0fb43723a7619c7bcbea61e04fd083
Kryštof Řezáč <krystofrezac@gmail.com>
2020-09-09 11:56:29 +0200

    Finish userDetail

--- app/src/pages/users/userDetail/personalInformation/index.tsx
+++ app/src/pages/users/userDetail/personalInformation/index.tsx
@@ -15,6 +15,54 @@ import EditIcon from '@material-ui/icons/Edit';
 import Tabs from 'components/Tabs';
 
 const PersonalInformationIndex: React.FC = () => {
+  const Info = (
+    <List>
+      <ListItem>
+        <ListItemText primary="Email" secondary="adam.janovsky@gmail.com" />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Telefonní číslo" secondary="+420 777 417 416" />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Číslo OP" secondary="+420 777 417 416" />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Datum narození" secondary="3.6.2001" />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+      <ListItem>
+        <ListItemText
+          primary="Trvalé bydliště"
+          secondary="Příčná ulice - Praha"
+        />
+        <ListItemSecondaryAction>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    </List>
+  );
+
   return (
     <Paper>
       <Box mb={2}>
@@ -26,70 +74,10 @@ const PersonalInformationIndex: React.FC = () => {
           {
             id: 0,
             label: 'Žák',
-            panel: (
-              <>
-                <List>
-                  <ListItem>
-                    <ListItemText
-                      primary="Email"
-                      secondary="adam.janovsky@gmail.com"
-                    />
-                    <ListItemSecondaryAction>
-                      <IconButton>
-                        <EditIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText
-                      primary="Telefonní číslo"
-                      secondary="+420 777 417 416"
-                    />
-                    <ListItemSecondaryAction>
-                      <IconButton>
-                        <EditIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText
-                      primary="Číslo OP"
-                      secondary="+420 777 417 416"
-                    />
-                    <ListItemSecondaryAction>
-                      <IconButton>
-                        <EditIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText
-                      primary="Datum narození"
-                      secondary="3.6.2001"
-                    />
-                    <ListItemSecondaryAction>
-                      <IconButton>
-                        <EditIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText
-                      primary="Trvalé bydliště"
-                      secondary="Příčná ulice - Praha"
-                    />
-                    <ListItemSecondaryAction>
-                      <IconButton>
-                        <EditIcon />
-                      </IconButton>
-                    </ListItemSecondaryAction>
-                  </ListItem>
-                </List>
-              </>
-            ),
+            panel: Info,
           },
-          { id: 1, label: 'Otec', panel: <div>ahoj</div> },
-          { id: 2, label: 'Matka', panel: <div>ahoj</div> },
+          { id: 1, label: 'Otec', panel: Info },
+          { id: 2, label: 'Matka', panel: Info },
         ]}
         variant="fullWidth"
       />
--- app/src/pages/users/userDetail/userDetail.tsx
+++ app/src/pages/users/userDetail/userDetail.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
 
-import { Grid } from '@material-ui/core';
+import { Box, Grid, Hidden } from '@material-ui/core';
 
-import GeneralInformationIndex from './generalInformation';
-import PersonalInformationIndex from './personalInformation';
+import GeneralInformation from './generalInformation';
+import Groups from './groups';
+import PersonalInformation from './personalInformation';
+import Subjects from './subjects';
 
 const UserDetail: React.FC = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={3}>
-        <GeneralInformationIndex />
+    <>
+      <Grid container spacing={2}>
+        <Grid item xl={3} md={6} xs={12}>
+          <Box display="flex" flexDirection="column">
+            <Box mb={2}>
+              <GeneralInformation />
+            </Box>
+            <Hidden xlUp>
+              <PersonalInformation />
+            </Hidden>
+          </Box>
+        </Grid>
+        <Hidden lgDown>
+          <Grid item xl={3} lg={6}>
+            <PersonalInformation />
+          </Grid>
+        </Hidden>
+        <Grid item xl={3} md={6} xs={12}>
+          <Box display="flex" flexDirection="column">
+            <Box mb={2}>
+              <Groups />
+            </Box>
+            <Hidden xlUp>
+              <Subjects />
+            </Hidden>
+          </Box>
+        </Grid>
+        <Hidden lgDown>
+          <Grid item xl={3}>
+            <Subjects />
+          </Grid>
+        </Hidden>
       </Grid>
-      <Grid item xs={3}>
-        <PersonalInformationIndex />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
