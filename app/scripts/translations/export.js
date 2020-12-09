257640c15352e7ab74cab79e2bce6d2f6e47dbba
Kryštof Řezáč <krystofrezac@gmail.com>
2020-12-09 15:14:18 +0100

    Fix tranlstions export

--- app/scripts/translations/export.js
+++ app/scripts/translations/export.js
@@ -4,43 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const json2csv = require('json2csv');
 
-const { languages, defaultLanguage } = i18n;
-const translationsFolder = path.join(__dirname, '../../public/static/locales');
+const { languages } = i18n;
+const parseData = require('./parseData');
 
-const parserData = [];
-const nestIn = (obj, path, language) => {
-  for (const key of Object.keys(obj)) {
-    const field = obj[key];
-    const fieldPath = `${path || ''}${path ? '.' : ''}${key}`;
-    if (typeof field === 'string') {
-      const currentIndex = parserData.findIndex(d => d.name === fieldPath);
-      if (currentIndex === -1) {
-        parserData.push({ name: fieldPath, [language]: field });
-      } else {
-        parserData[currentIndex] = {
-          ...parserData[currentIndex],
-          [language]: field,
-        };
-      }
-    } else {
-      nestIn(field, fieldPath, language);
-    }
-  }
-};
-
-for (const language of Object.keys(languages)) {
-  const files = fs.readdirSync(path.join(translationsFolder, language));
-
-  for (const file of files) {
-    const fileData = fs.readFileSync(
-      path.join(translationsFolder, defaultLanguage, file),
-    );
-
-    nestIn(JSON.parse(fileData), file.replace('.json', ''), language);
-  }
-}
-
-const csvData = json2csv.parse(parserData, {
+const csvData = json2csv.parse(parseData(), {
   fields: ['name', ...Object.keys(languages)],
 });
 fs.writeFileSync(
