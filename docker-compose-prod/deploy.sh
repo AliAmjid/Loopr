7b3b2c644c5b927c62e094c9e955e1eed9e2e98e
Ali Amjid <alih.amjid126@gmail.com>
2020-12-20 14:46:24 +0100

    fix

--- docker-compose-prod/deploy.sh
+++ docker-compose-prod/deploy.sh
@@ -1,4 +1,5 @@
 cd /var/www/loopr
+gcloud --quiet auth configure-docker
 docker-compose -f ./docker-compose-prod/docker-compose.yml down
 docker-compose -f ./docker-compose-prod/docker-compose.yml pull
 docker-compose -f docker-compose-prod/docker-compose.yml up -d
