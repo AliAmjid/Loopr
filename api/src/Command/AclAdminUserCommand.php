3dc450607ffb0e4201f6952ed0e5c88f0f53ad3f
Ali Amjid <alih.amjid126@gmail.com>
2020-12-20 16:51:46 +0100

    fix

--- api/src/Command/AclAdminUserCommand.php
+++ api/src/Command/AclAdminUserCommand.php
@@ -2,22 +2,25 @@
 
 namespace App\Command;
 
+use App\Entity\AclResource;
+use App\Entity\AclRole;
+use App\Entity\User;
 use App\Enum\AclResourceEnum;
-use App\Tests\Helpers\TCreateEntityHelpers;
 use Doctrine\Persistence\ManagerRegistry;
+use Nette\Utils\Random;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
 
 class AclAdminUserCommand extends Command
 {
-    use TCreateEntityHelpers;
 
     protected $kernel;
+    protected $em;
 
     protected static $defaultName = 'acl:admin-user';
 
@@ -51,5 +54,49 @@ class AclAdminUserCommand extends Command
         $io->success('password: ' . $password);
         $io->success('Generated user');
         return Command::SUCCESS;
+    }
+
+    protected function createRandomUser(
+        $password = 'test',
+        $resources = AclResourceEnum::PROP_DEFAULT_ROLES['ROLE_USER'],
+        $email = null
+    ): User {
+        $user = new User();
+        $user->setEmail($email ?? Random::generate(4) . "@loopr.cz");
+        $user->setFirstname(Random::generate());
+        /** @var UserPasswordEncoder $encoder */
+        $encoder = $this->kernel->getContainer()->get('security.password_encoder');
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setFirstname(Random::generate());
+        $user->setLastname(Random::generate());
+        $user->setRole($this->createRoleWithResources($resources));
+        $this->em->persist($user);
+        $this->em->flush();
+        return $user;
+    }
+
+
+    protected function createRoleWithResources(array $resources)
+    {
+        $role = new AclRole();
+        $role->setName($this->randomRoleName());
+        foreach ($resources as $resource) {
+            /** @var AclResource $resource */
+            $resource = $this->em->find(AclResource::class, AclResourceEnum::PROP_UUIDS[$resource]);
+            if ($resource) {
+                $role->getResources()->add($resource);
+            }
+            foreach ($resource->getDependsOn() as $parent) {
+                $role->addResource($parent);
+            }
+        }
+        $this->em->persist($role);
+        $this->em->flush();
+        return $role;
+    }
+
+    protected function randomRoleName(): string
+    {
+        return 'ROLE_' . strtoupper(Random::generate(5, 'a-z'));
     }
 }
