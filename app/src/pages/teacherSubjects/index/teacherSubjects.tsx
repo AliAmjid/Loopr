8e50587528d5d31f90039d713099b3f9f5531eb8
Kryštof Řezáč <krystofrezac@gmail.com>
2020-12-31 14:55:29 +0100

    Working on fucking evaluation table

--- app/src/pages/teacherSubjects/index/teacherSubjects.tsx
+++ app/src/pages/teacherSubjects/index/teacherSubjects.tsx
@@ -9,18 +9,29 @@ import {
   Grid,
   makeStyles,
   Paper,
+  Theme,
   Typography,
 } from '@material-ui/core';
+import Link from 'next/link';
 
 import routes from 'config/routes';
 
-import Link from 'components/Link';
 import ThickDivider from 'components/thickDivider';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    padding: 0,
+  },
+  cardInnerSpacer: {
+    padding: theme.spacing(1),
+  },
   cardActions: {
     display: 'flex',
     justifyContent: 'center',
+  },
+  colorStrip: {
+    width: '100%',
+    height: theme.spacing(2),
   },
 }));
 
@@ -29,15 +40,21 @@ const TeacherSubjects: React.FC = () => {
 
   const card = (
     <Grid item xs={3}>
-      <Card variant="outlined">
-        <CardContent>
-          <Typography variant="h5">1.B</Typography>
-        </CardContent>
-        <CardActions className={classes.cardActions}>
-          <Link href={routes.teacherSubjects.subject.index} passHref>
-            <Button color="primary">Evaluation</Button>
-          </Link>
-        </CardActions>
+      <Card variant="outlined" className={classes.card}>
+        <div
+          className={classes.colorStrip}
+          style={{ backgroundColor: 'red' }}
+        />
+        <div className={classes.cardInnerSpacer}>
+          <CardContent>
+            <Typography variant="h5">1.B</Typography>
+          </CardContent>
+          <CardActions className={classes.cardActions}>
+            <Link href={routes.teacherSubjects.subject.index} passHref>
+              <Button color="primary">Evaluation</Button>
+            </Link>
+          </CardActions>
+        </div>
       </Card>
     </Grid>
   );
--- app/src/pages/teacherSubjects/subject/subject.tsx
+++ app/src/pages/teacherSubjects/subject/subject.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { MutableRefObject, useRef } from 'react';
 
 import {
+  Grid,
   IconButton,
   makeStyles,
   Paper,
   Table,
   TableBody,
   TableCell,
+  TableContainer,
   TableHead,
   TableRow,
   Typography,
@@ -14,101 +16,211 @@ import {
 } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
 
-const useStyles = makeStyles({
-  withoutBorders: {
-    borderBottom: 'none !important',
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
   },
-  head: {
-    backgroundColor: '#fff',
-    minWidth: '50px',
+  tableContainer: {
+    maxHeight: '400px',
   },
-});
+  cell: {
+    minWidth: '100px',
+  },
+  studentCell: {
+    minWidth: '200px',
+  },
+}));
 
 const StickyTableCell = withStyles(theme => ({
   head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
     left: 0,
     position: 'sticky',
     zIndex: theme.zIndex.appBar + 2,
   },
   body: {
     backgroundColor: '#ddd',
-    minWidth: '50px',
     left: 0,
     position: 'sticky',
     zIndex: theme.zIndex.appBar + 1,
   },
 }))(TableCell);
 
+const StyledTableCell = withStyles(theme => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
 const Subject: React.FC = () => {
+  let id = 0;
+  function createData(name, calories, fat, carbs, protein) {
+    id += 1;
+
+    return { id, name, calories, fat, carbs, protein };
+  }
+
+  const data = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
   const classes = useStyles();
+  const headerRef = useRef<HTMLTableRowElement>() as MutableRefObject<
+    HTMLTableRowElement
+  >;
 
   return (
     <Paper>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <StickyTableCell rowSpan={2} className={classes.head}>
-              <TableCell>Jméno</TableCell>
-              <TableCell>Příjmení</TableCell>
-            </StickyTableCell>
+      <TableContainer className={classes.tableContainer}>
+        <Table stickyHeader>
+          <TableHead>
+            <TableRow ref={headerRef}>
+              <StickyTableCell
+                rowSpan={2}
+                className={`${'head' + ' '}${classes.studentCell}`}
+              >
+                <Grid container>
+                  <Grid item xs={6}>
+                    Jméno
+                  </Grid>
+                  <Grid item xs={6}>
+                    Příjmení
+                  </Grid>
+                </Grid>
+              </StickyTableCell>
 
-            <TableCell
-              colSpan={2}
-              align="center"
-              className={classes.withoutBorders}
-            >
-              <Typography>Test1</Typography>
-              <Typography>10 bodů</Typography>
-              <IconButton color="primary">
-                <EditIcon />
-              </IconButton>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell align="center">
-              <Typography>BODY</Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography>PROCENTA</Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <StickyTableCell>
-              <TableCell>
-                <Typography>Jan</Typography>
+              <TableCell
+                colSpan={2}
+                align="center"
+                // className={classes.withoutBorders}
+              >
+                <Typography>Test1</Typography>
+                <Typography>10 bodů</Typography>
+                <IconButton color="primary">
+                  <EditIcon />
+                </IconButton>
               </TableCell>
-              <TableCell>
-                <Typography>Jan</Typography>
+            </TableRow>
+            <TableRow>
+              <TableCell
+                align="center"
+                style={{ top: headerRef.current?.clientHeight }}
+              >
+                <Typography>BODY</Typography>
               </TableCell>
-            </StickyTableCell>
-
-            <TableCell align="center">
-              <Typography>1</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography align="center">10%</Typography>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <StickyTableCell>
-              <Typography>Jan</Typography>
-            </StickyTableCell>
-            <TableCell>
-              <Typography>Novák</Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography>1</Typography>
-            </TableCell>
-            <TableCell>
-              <Typography align="center">10%</Typography>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+              <TableCell
+                align="center"
+                style={{ top: headerRef.current?.clientHeight }}
+              >
+                <Typography>PROCENTA</Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map(n => {
+              return (
+                <StyledTableRow key={n.id}>
+                  <StickyTableCell className="body">
+                    <Grid container>
+                      <Grid item xs={6}>
+                        basdksodfhofsdhf
+                      </Grid>
+                      <Grid item xs={6}>
+                        a
+                      </Grid>
+                    </Grid>
+                  </StickyTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.fat}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.carbs}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.calories}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.fat}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.carbs}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.calories}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.fat}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.carbs}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.carbs}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.calories}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.fat}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.carbs}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                  <StyledTableCell align="center" className={classes.cell}>
+                    {n.protein}
+                  </StyledTableCell>
+                </StyledTableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
   );
 };
