1697219cb9426e37cc485c64d6445bc3c15f15cf
Ali Amjid <alih.amjid126@gmail.com>
2021-03-03 20:04:59 +0100

    feat: lot of stuff

---
+++ api/src/Controller/GetCurrentHalfYearSchoolPeriodController.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use ApiPlatform\Core\GraphQl\Resolver\QueryCollectionResolverInterface;
+use App\Entity\SchoolPeriod;
+use App\Error\ClientError;
+use App\Error\ClientErrorType;
+use App\Helper\IriHelper;
+use App\Repository\SchoolPeriodRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetForSubjectSchoolPeriodController extends AbstractController implements QueryCollectionResolverInterface
+{
+
+    public function __invoke(iterable $collection, array $context): iterable
+    {
+        $idSubject = IriHelper::getIdFromIri($collection['args']['idSubject']);
+        $em = $this->getDoctrine()->getManager();
+        /** @var SchoolPeriodRepository $schoolPeriodRepository */
+        $schoolPeriodRepository = $em->getRepository(SchoolPeriod::class);
+        $active = $schoolPeriodRepository->findActive();
+        if (!$active) {
+            throw new ClientError(ClientErrorType::NO_SCHOOL_PERIOD_ACTIVE);
+        }
+
+        if ($active->getQuarter() % 2 === 0) {
+            $otherPeriod = $schoolPeriodRepository->findByQuarterNYear(
+                $active->getQuarter() - 1,
+                $active->getSchoolYear()
+            );
+            return [
+                $otherPeriod,
+                $active,
+            ];
+        } else {
+            $otherPeriod = $schoolPeriodRepository->findByQuarterNYear(
+                $active->getQuarter() + 1,
+                $active->getSchoolYear()
+            );
+            return [
+                $active,
+                $otherPeriod
+            ];
+        }
+    }
+
+}
