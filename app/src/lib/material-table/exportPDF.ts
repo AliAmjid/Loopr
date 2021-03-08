f8dae2d01beb4cad2bc3f0f77f162f1c48c6ba96
Kryštof Řezáč <krystofrezac@gmail.com>
2021-03-08 18:35:36 +0100

    Fix user pdf export

--- app/src/lib/material-table/exportPDF.ts
+++ app/src/lib/material-table/exportPDF.ts
@@ -8,12 +8,14 @@ const exportPDF = (columns: Column<any>[], data: any[]): void => {
   const filteredColumns: {
     field: string;
     title: string;
+    lookup: Record<any, any>;
   }[] = [];
   columns.forEach(column => {
     if (typeof column.field === 'string' && typeof column.title === 'string') {
       filteredColumns.push({
         field: column.field,
         title: column.title,
+        lookup: column.lookup,
       });
     }
   });
@@ -36,10 +38,15 @@ const exportPDF = (columns: Column<any>[], data: any[]): void => {
                 const fieldSteps = field.split('.');
                 value = row;
                 fieldSteps.forEach(step => {
-                  value = value[step];
+                  console.log(value, step);
+                  if (value) value = value[step];
                 });
 
-                return value;
+                if (c.lookup) {
+                  value = c.lookup[value];
+                }
+
+                return value || '';
               }),
             ),
           ],
