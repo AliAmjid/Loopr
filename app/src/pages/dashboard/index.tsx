c1ede3db8c52a522d177cccdc492a408d5890839
Kryštof Řezáč <krystofrezac@gmail.com>
2021-03-19 14:40:28 +0100

    Fix bugs

--- app/src/pages/dashboard/index.tsx
+++ app/src/pages/dashboard/index.tsx
@@ -18,9 +18,11 @@ const DashboardIndex: React.FC = () => {
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={8} hidden={!canStudy}>
-          <ExamsIndex />
-        </Grid>
+        {canStudy && (
+          <Grid item xs={12} md={8}>
+            <ExamsIndex />
+          </Grid>
+        )}
         <Grid item xs={12} md={4}>
           <NotificationsIndex />
         </Grid>
--- app/src/pages/login/index/index.tsx
+++ app/src/pages/login/index/index.tsx
@@ -77,7 +77,7 @@ const LoginIndex: React.FC = () => {
     }
   };
 
-  if (meUserData && !meUserError) {
+  if (meUserData?.meUser && !meUserError) {
     automaticallyLogIn();
   }
 
